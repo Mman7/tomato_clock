7c7406b01d4e941a86ab578e3b5db35339ebcaf0
Mackenzie <youzai0427@gmail.com>
2022-01-07 15:57:46 +0800

    update✅ add text Tehme

--- lib/src/layouts/theme.dart
+++ lib/src/layouts/theme.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 
 var themeData = ThemeData(
-  primaryColorDark: HexColor('#125E2B'),
-  primaryColor: HexColor('#4BAF66'),
-);
+    primaryColorDark: HexColor('#125E2B'),
+    primaryColor: HexColor('#4BAF66'),
+    textTheme: TextTheme(bodyText1: TextStyle(color: HexColor('#0D4721'))));
