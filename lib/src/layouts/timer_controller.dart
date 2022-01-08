73c6a7920bf0d64bc06836224c8e21cec1c24705
Mackenzie <youzai0427@gmail.com>
2022-01-08 17:16:44 +0800

    new 📁 timer controller layout

---
+++ lib/src/layouts/timer_controller.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tomato_clock/src/providers/current_status_provider.dart';
+import 'package:tomato_clock/src/providers/tomato_providers.dart';
+
+import 'timer_control_card.dart';
+
+class TimerController extends StatelessWidget {
+  const TimerController({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var _status = context.watch<CurrentStatus>().status;
+    double statusChecker(value) {
+      if (_status == null) return 1;
+      return _status == value ? 1 : 0.5;
+    }
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          Opacity(
+            opacity: statusChecker('focus'),
+            child: TimerControlCard(
+              title: 'Focus',
+              countMinute: 25,
+              onStart: () =>
+                  context.read<CurrentStatus>().changeStatus(value: 'focus'),
+              onFinish: () {
+                context.read<TomatoCount>().increaseTomatoCount();
+                context.read<CurrentStatus>().changeStatus(value: 'rest');
+              },
+            ),
+          ),
+          const SizedBox(
+            width: 20,
+          ),
+          Opacity(
+            opacity: statusChecker('rest'),
+            child: TimerControlCard(
+              title: 'Rest',
+              countMinute: 5,
+              onStart: () =>
+                  context.read<CurrentStatus>().changeStatus(value: 'rest'),
+              onFinish: () {
+                context.read<CurrentStatus>().changeStatus(value: 'focus');
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
