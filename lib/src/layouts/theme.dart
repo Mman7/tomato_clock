be1ff6279b6f720d64210d512ff7adf604d1030a
Mackenzie <youzai0427@gmail.com>
2022-01-12 01:04:25 +0800

    Update✅ add dialog Theme

--- lib/src/layouts/theme.dart
+++ lib/src/layouts/theme.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 
-var themeData = ThemeData(
-    primaryColorDark: HexColor('#125E2B'),
-    primaryColor: HexColor('#4BAF66'),
-    textTheme: TextTheme(bodyText1: TextStyle(color: HexColor('#0D4721'))));
+ThemeData theme(BuildContext context) {
+  return ThemeData(
+      primaryColorDark: HexColor('#125E2B'),
+      primaryColor: HexColor('#4BAF66'),
+      dialogTheme: DialogTheme(
+          titleTextStyle: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+              color: Theme.of(context).primaryColor),
+          contentTextStyle: TextStyle(
+              fontSize: 17,
+              color: Theme.of(context).textTheme.bodyText1?.color),
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10)))),
+      textTheme: TextTheme(bodyText1: TextStyle(color: HexColor('#0D4721'))));
+}
