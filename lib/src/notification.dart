cc36c3a2adb6c05e74dc572112a42e97adeeface
Mackenzie <youzai0427@gmail.com>
2022-01-12 01:04:40 +0800

    Add📁✅ Notificationer

---
+++ lib/src/notification.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class NotificationService extends ChangeNotifier {
+  final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  //initilize
+  Future initialize() async {
+    FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+        FlutterLocalNotificationsPlugin();
+    AndroidInitializationSettings androidInitializationSettings =
+        const AndroidInitializationSettings('ic_launcher');
+
+    IOSFlutterLocalNotificationsPlugin iosFlutterLocalNotificationsPlugin =
+        IOSFlutterLocalNotificationsPlugin();
+
+    final InitializationSettings initializationSettings =
+        InitializationSettings(android: androidInitializationSettings);
+
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  }
+
+  // Instant Notifications
+  Future instantNotification() async {
+    var android = const AndroidNotificationDetails('id', 'channel');
+
+    var ios = const IOSNotificationDetails();
+
+    var platform = NotificationDetails(android: android, iOS: ios);
+
+    await _flutterLocalNotificationsPlugin.show(
+        0, "Time's up Buddy !", 'Tap here recount your time', platform,
+        payload: 'Welcome to demo app');
+  }
+}
