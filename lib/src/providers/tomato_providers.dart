4a5ce281fc9ef5906f2e56c8a412925daf899c98
Mackenzie <youzai0427@gmail.com>
2022-01-12 19:09:23 +0800

    remove❌ unsed code

--- lib/src/providers/tomato_providers.dart
+++ lib/src/providers/tomato_providers.dart
@@ -6,23 +6,15 @@ class TomatoCount with ChangeNotifier {
   int get tomatoCount => _tomatoCount;
 
   increaseTomatoCount() {
-    print('completed 1 tomato now rest');
     _tomatoCount++;
-    if (_tomatoCount == 4) restTimeNow();
-    //TODO Add tomato to history
-    // history need to save
-    //
+
     notifyListeners();
   }
 
-  _cleanTomatoCount() {
+  addTomatoCountToHistory() {}
+
+  cleanTomatoCount() {
     _tomatoCount = 0;
-    notifyListeners();
-  }
-
-  restTimeNow() {
-    print('ITS REST TIME BITCH');
-    _cleanTomatoCount();
     notifyListeners();
   }
 }
