d3f8bbaeb72651848f43099fb15fb69f7d33bf43
Mackenzie <youzai0427@gmail.com>
2022-01-15 03:01:57 +0800

    update✅ add tips

--- lib/src/layouts/history_displayer.dart
+++ lib/src/layouts/history_displayer.dart
@@ -16,54 +16,55 @@ class _HistoryDisplayerState extends State<HistoryDisplayer> {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-        child: RefreshIndicator(
-      onRefresh: () async {
-        setState(() {});
-      },
-      child: FutureBuilder<dynamic>(
-        future: context.read<TomatoDataBase>().fetchData(),
-        builder: (context, AsyncSnapshot<dynamic> snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: ListView.builder(
-                  padding: EdgeInsets.zero,
-                  itemCount: snapshot.data?.length ?? 0,
-                  itemBuilder: (context, index) {
-                    if (snapshot.data == null) return Container();
-                    var tomatoCount = snapshot.data?[index]['tomatoCount'];
-                    var date = snapshot.data?[index]['date'];
-                    var svgHeight = MediaQuery.of(context).size.height / 15;
-                    return Column(
-                      children: [
-                        CustomGradientBackground(
-                          firstColor: "#88FFA7",
-                          secondColor: '#3A754A',
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
-                          borderRadius:
-                              const BorderRadius.all(Radius.circular(10)),
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 5),
-                            child: TomatoCard(
-                                date: date,
-                                svgHeight: svgHeight,
-                                tomatoCount: tomatoCount),
-                          ),
+        child: FutureBuilder<dynamic>(
+      future: context.read<TomatoDataBase>().fetchData(),
+      builder: (context, AsyncSnapshot<dynamic> snapshot) {
+        Widget textTips = const Text(
+          'Pull down to refresh',
+          style: TextStyle(color: Colors.grey),
+        );
+        if (snapshot.data == null) return textTips;
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: ListView.builder(
+                padding: EdgeInsets.zero,
+                itemCount: snapshot.data?.length ?? 0,
+                itemBuilder: (context, index) {
+                  if (snapshot.data == null) return textTips;
+                  var tomatoCount = snapshot.data?[index]['tomatoCount'];
+                  var date = snapshot.data?[index]['date'];
+                  var svgHeight = MediaQuery.of(context).size.height / 15;
+                  return Column(
+                    children: [
+                      CustomGradientBackground(
+                        firstColor: "#88FFA7",
+                        secondColor: '#3A754A',
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(10)),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 5),
+                          child: TomatoCard(
+                              date: date,
+                              svgHeight: svgHeight,
+                              tomatoCount: tomatoCount),
                         ),
-                        const Divider(
-                          height: 30,
-                          thickness: 2,
-                        )
-                      ],
-                    );
-                  }),
-            );
-          } else {
-            return const CircularProgressIndicator();
-          }
-        },
-      ),
+                      ),
+                      const Divider(
+                        height: 30,
+                        thickness: 2,
+                      ),
+                      textTips
+                    ],
+                  );
+                }),
+          );
+        } else {
+          return const CircularProgressIndicator();
+        }
+      },
     ));
   }
 }
