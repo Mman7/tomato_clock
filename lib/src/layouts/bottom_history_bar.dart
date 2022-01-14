252a8d15b61e41af91f1a4baa77fb112ef918d3d
Mackenzie <youzai0427@gmail.com>
2022-01-15 03:02:37 +0800

    fix ✅ bottom history bar not responsive

--- lib/src/layouts/bottom_history_bar.dart
+++ lib/src/layouts/bottom_history_bar.dart
@@ -10,9 +10,10 @@ class BottomHistoryBar extends StatefulWidget {
 
 class _BottomHistoryBarState extends State<BottomHistoryBar> {
   bool isHistoryBasOpen = false;
-  historyBarValue() => isHistoryBasOpen
-      ? MediaQuery.of(context).size.height / 10
-      : MediaQuery.of(context).size.height / 1.27;
+  late double height = MediaQuery.of(context).size.height;
+  late double width = MediaQuery.of(context).size.width;
+  late final appBar = AppBar().preferredSize;
+  historyBarValue() => isHistoryBasOpen ? height / 1.26 : appBar.height;
 
   @override
   Widget build(BuildContext context) {
@@ -20,20 +21,18 @@ class _BottomHistoryBarState extends State<BottomHistoryBar> {
     double width = MediaQuery.of(context).size.width;
     return AnimatedPositioned(
       curve: Curves.fastLinearToSlowEaseIn,
-      height: MediaQuery.of(context).size.height,
-      width: MediaQuery.of(context).size.width,
-      top: historyBarValue(),
+      height: historyBarValue(),
+      width: width,
+      bottom: 01,
       duration: const Duration(seconds: 1),
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: width / 45, vertical: 0),
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-          decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.vertical(top: Radius.circular(10))),
-          child: Column(
-            children: [customButton(primaryColor), const HistoryDisplayer()],
-          ),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.vertical(top: Radius.circular(10))),
+        child: Column(
+          children: [customButton(primaryColor), const HistoryDisplayer()],
         ),
       ),
     );
@@ -44,18 +43,24 @@ class _BottomHistoryBarState extends State<BottomHistoryBar> {
         onPressed: () {
           setState(() => isHistoryBasOpen = !isHistoryBasOpen);
         },
-        child: Row(
-          children: [
-            Text(
-              'History ',
-              style: TextStyle(color: primaryColor, fontSize: 20),
-            ),
+        child:
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Text(
+            'History ',
+            style: TextStyle(color: primaryColor, fontSize: 20),
+          ),
+          if (isHistoryBasOpen)
             Icon(
               Icons.keyboard_arrow_down,
               color: primaryColor,
               size: 30,
             )
-          ],
-        ));
+          else
+            Icon(
+              Icons.keyboard_arrow_up,
+              color: primaryColor,
+              size: 30,
+            ),
+        ]));
   }
 }
