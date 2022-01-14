7b0099f590687b4fcf1e9374abf7ddba64e1d88f
Mackenzie <youzai0427@gmail.com>
2022-01-14 20:01:28 +0800

    commet © ununsed code

--- lib/src/tomato_database.dart
+++ lib/src/tomato_database.dart
@@ -3,54 +3,49 @@ import 'package:intl/intl.dart';
 import 'package:localstore/localstore.dart';
 
 class TomatoDataBase with ChangeNotifier {
-  //TODO Refactor
   final _db = Localstore.instance;
 
   String formatDate(DateTime date) => DateFormat("MMMM d yyyy").format(date);
 
-  //TODO Change todos to tomatoWithDate
   Future<dynamic> fetchData() async {
-    var items = await _db.collection('todos').get();
+    var items = await _db.collection('tomato').get();
     var parseItem = items?.entries.map((e) => e.value).toList();
     return Future.value(parseItem);
   }
 
-  printAllData() async {
-    var items = await _db.collection('todos').get();
-    print(items);
-  }
+  // printAllData() async {
+  //   var items = await _db.collection('tomato').get();
+  //   print(items);
+  // }
 
   saveTomato() {
     _db
-        .collection('todos')
+        .collection('tomato')
         .doc(formatDate(DateTime.now()))
         .set({'tomatoCount': 1, 'date': formatDate(DateTime.now())});
-    print('saved');
     notifyListeners();
   }
 
-  findDataById(DateTime id) async {
-    var item = await _db.collection('todos').doc(formatDate(id)).get();
-    var parseItem = item?.entries.map((e) => e.value);
-    print(parseItem);
-  }
+  // findDataById(DateTime id) async {
+  //   var item = await _db.collection('tomato').doc(formatDate(id)).get();
+  //   var parseItem = item?.entries.map((e) => e.value);
+  // }
 
   /// id = DateTime.now()
   increaseTomatoData() async {
     var id = formatDate(DateTime.now());
-    var item = await _db.collection('todos').doc(id).get();
+    var item = await _db.collection('tomato').doc(id).get();
     var tomatoCountInData = item?.entries.first.value;
 
     if (tomatoCountInData == null) {
       saveTomato();
     } else {
-      _db.collection('todos').doc(id).delete();
-      _db.collection('todos').doc(id).set({
+      _db.collection('tomato').doc(id).delete();
+      _db.collection('tomato').doc(id).set({
         'tomatoCount': tomatoCountInData + 1,
         'date': formatDate(DateTime.now())
       });
     }
-    print('increased tomato from database');
     notifyListeners();
   }
 }
