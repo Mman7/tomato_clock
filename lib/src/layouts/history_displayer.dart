21c7f4472ddd6f2cc03aa443d28f30114fb0a546
MackMan <youzai0427@gmail.com>
2022-01-29 00:48:29 +0800

    update ✅refactor code

--- lib/src/layouts/history_displayer.dart
+++ lib/src/layouts/history_displayer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:responsive_flutter/responsive_flutter.dart';
 
 import '../providers/tomato_providers.dart';
 import '../tomato_database.dart';
@@ -7,64 +8,80 @@ import 'custom_gradient_background.dart';
 import 'history_tomato_card.dart';
 
 class HistoryDisplayer extends StatefulWidget {
-  const HistoryDisplayer({Key? key, required}) : super(key: key);
-
+  const HistoryDisplayer({Key? key, required this.controller})
+      : super(key: key);
+  final controller;
   @override
   State<HistoryDisplayer> createState() => _HistoryDisplayerState();
 }
 
 class _HistoryDisplayerState extends State<HistoryDisplayer> {
+  late Future _getdata;
+  @override
+  void initState() {
+    super.initState();
+    _getdata = context.read<TomatoDataBase>().fetchData();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
     context.read<TomatoCount>().addListener(() {
       setState(() {});
     });
-    return Expanded(
-        child: FutureBuilder<dynamic>(
-      future: context.read<TomatoDataBase>().fetchData(),
+    return FutureBuilder<dynamic>(
+      future: _getdata,
       builder: (context, AsyncSnapshot<dynamic> snapshot) {
         if (snapshot.data == null) return Container();
-        if (snapshot.connectionState == ConnectionState.done) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: ListView.builder(
-                physics: const AlwaysScrollableScrollPhysics(),
-                padding: EdgeInsets.zero,
-                itemCount: snapshot.data?.length ?? 0,
-                itemBuilder: (context, index) {
-                  if (snapshot.data == null) return Container();
-                  var tomatoCount = snapshot.data?[index]['tomatoCount'];
-                  var date = snapshot.data?[index]['date'];
-                  var svgHeight = MediaQuery.of(context).size.height / 15;
-                  return Column(
-                    children: [
-                      CustomGradientBackground(
-                        firstColor: "#88FFA7",
-                        secondColor: '#3A754A',
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(10)),
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 5),
-                          child: TomatoCard(
-                              date: date,
-                              svgHeight: svgHeight,
-                              tomatoCount: tomatoCount),
-                        ),
-                      ),
-                      const Divider(
-                        height: 30,
-                        thickness: 2,
-                      ),
-                    ],
+        if (snapshot.hasData) {
+          return ListView.separated(
+              padding: EdgeInsets.zero,
+              separatorBuilder: (context, index) => const Divider(
+                    height: 30,
+                    thickness: 2,
+                  ),
+              controller: widget.controller,
+              itemCount: snapshot.data?.length ?? 0,
+              itemBuilder: (context, index) {
+                if (snapshot.data == null) return Container();
+                var tomatoCount = snapshot.data?[index]['tomatoCount'];
+                var date = snapshot.data?[index]['date'];
+                if (index == 0) {
+                  return ListTile(
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: height / 100),
+                    leading: Text(
+                      'History Bar',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          color: Theme.of(context).primaryColorDark,
+                          fontSize:
+                              ResponsiveFlutter.of(context).fontSize(2.75)),
+                    ),
+                    trailing: Icon(
+                      Icons.drag_handle,
+                      size: ResponsiveFlutter.of(context).fontSize(3.75),
+                      color: Theme.of(context).primaryColorDark,
+                    ),
                   );
-                }),
-          );
+                }
+                return Column(
+                  children: [
+                    CustomGradientBackground(
+                      firstColor: "#88FFA7",
+                      secondColor: '#3A754A',
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      borderRadius: const BorderRadius.all(Radius.circular(10)),
+                      child: TomatoCard(date: date, tomatoCount: tomatoCount),
+                    ),
+                  ],
+                );
+              });
         } else {
-          return const CircularProgressIndicator();
+          return ListView();
         }
       },
-    ));
+    );
   }
 }
