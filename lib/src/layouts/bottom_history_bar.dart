b829541bbbe70708329ede287806a70dbf75610d
MackMan <youzai0427@gmail.com>
2022-01-29 00:47:36 +0800

    update ✅simplify bottom history, now using drag to open

--- lib/src/layouts/bottom_history_bar.dart
+++ lib/src/layouts/bottom_history_bar.dart
@@ -10,63 +10,28 @@ class BottomHistoryBar extends StatefulWidget {
 
 class _BottomHistoryBarState extends State<BottomHistoryBar> {
   bool isHistoryBasOpen = false;
-  late double height = MediaQuery.of(context).size.height;
-  late double width = MediaQuery.of(context).size.width;
   late final appBar = AppBar().preferredSize;
-  historyBarValue() => isHistoryBasOpen ? height / 1.4 : appBar.height * 1.04;
 
   @override
   Widget build(BuildContext context) {
-    Color primaryColor = Theme.of(context).primaryColorDark;
     double width = MediaQuery.of(context).size.width;
-    return AnimatedPositioned(
-      curve: Curves.fastLinearToSlowEaseIn,
-      height: historyBarValue(),
-      width: width,
-      bottom: 0,
-      duration: const Duration(seconds: 1),
-      child: Container(
-        margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 0),
-        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-        decoration: const BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.vertical(top: Radius.circular(10))),
-        child: Column(
-          children: [customButton(primaryColor), const HistoryDisplayer()],
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 15),
+      child: DraggableScrollableSheet(
+        initialChildSize: 0.1,
+        maxChildSize: 0.9,
+        minChildSize: 0.1,
+        builder: (context, controller) => Container(
+          padding: EdgeInsets.symmetric(horizontal: width / 25),
+          decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20), topRight: Radius.circular(20))),
+          child: HistoryDisplayer(
+            controller: controller,
+          ),
         ),
       ),
     );
-  }
-
-  TextButton customButton(Color primaryColor) {
-    var child1 = Row(
-      children: [
-        Text(
-          'History ',
-          style: TextStyle(color: primaryColor, fontSize: 20),
-        ),
-        if (isHistoryBasOpen)
-          Icon(
-            Icons.keyboard_arrow_down,
-            color: primaryColor,
-            size: 30,
-          )
-        else
-          Icon(
-            Icons.keyboard_arrow_up,
-            color: primaryColor,
-            size: 30,
-          ),
-      ],
-    );
-
-    return TextButton(
-        onPressed: () {
-          setState(() => isHistoryBasOpen = !isHistoryBasOpen);
-        },
-        child:
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-          child1,
-        ]));
   }
 }
