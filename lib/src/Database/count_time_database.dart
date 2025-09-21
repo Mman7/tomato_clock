38df1ecb66ddde7299d4449ef874d918fbb89652
Eric Man <youzai0427@gmail.com>
2025-09-22 04:30:46 +0800

    refactor

--- lib/src/Database/count_time_database.dart
+++ lib/src/Database/count_time_database.dart
@@ -1,19 +1,23 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class CountTime {
-  /// [databaseName] = FocusCountTime / RestCountTime
-  static saveTimer({required String databaseName, countTime}) async {
-    var database = await SharedPreferences.getInstance();
-    await database.setInt('${databaseName}CountTime', countTime);
-  }
+  static const _defaultFocusTime = 25;
+  static const _restCountTime = 5;
 
   /// [databaseName] = FocusCountTime / RestCountTime
-  static getTimer({databaseName}) async {
+  static saveTimer(
+      {required String databaseName, required int countTime}) async {
+    var database = await SharedPreferences.getInstance();
+    await database.setInt(databaseName, countTime);
+  }
+
+  static _defaultTime(databaseName) =>
+      databaseName == 'focus' ? _defaultFocusTime : _restCountTime;
+
+  /// [databaseName] = FocusCountTime / RestCountTime
+  static Future<int> getTimer({required String databaseName}) async {
     final database = await SharedPreferences.getInstance();
-    final data = database.getInt('${databaseName}CountTime');
-    const defaultFocusTime = 25;
-    const restCountTime = 5;
-    if (databaseName == 'Focus') return data ?? defaultFocusTime;
-    if (databaseName == 'Rest') return data ?? restCountTime;
+    int? data = database.getInt(databaseName) ?? _defaultTime(databaseName);
+    return data as int;
   }
 }
