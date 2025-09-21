53bd52078bcf4e88204f5388f6fe0f7f21ff10d5
Eric Man <youzai0427@gmail.com>
2025-09-22 05:13:27 +0800

    refactor: reduce code complexity

--- lib/src/Database/tomato_database.dart
+++ lib/src/Database/tomato_database.dart
@@ -2,32 +2,33 @@ import 'package:intl/intl.dart';
 import 'package:localstore/localstore.dart';
 
 class TomatoDataBase {
-  static final _db = Localstore.instance;
-  static String formatDate(DateTime date) =>
+  static final Localstore _db = Localstore.instance;
+  static String _formatDate(DateTime date) =>
       DateFormat("MMMM d yyyy").format(date);
 
   static Future<List> fetchData() async {
-    dynamic items = await _db.collection('tomato').get();
-    if (items == null) return [];
-    var parseItem = items?.entries.map((e) => e.value);
-    return Future.value(parseItem);
+    dynamic tomatoCollection = await _db.collection('tomato').get();
+    if (tomatoCollection == null) return [];
+    List parseItem = tomatoCollection.entries.map((e) => e.value).toList();
+    return parseItem;
   }
 
   static saveTomato() {
     _db
         .collection('tomato')
-        .doc(formatDate(DateTime.now()))
-        .set({'tomatoCount': 1, 'date': formatDate(DateTime.now())});
+        .doc('${DateTime.now()}')
+        .set({'tomatoCount': 1, 'date': _formatDate(DateTime.now())});
   }
 
   static addNewTomatoData() async {
-    var id = formatDate(DateTime.now());
+    // get today
+    String id = _formatDate(DateTime.now());
     var item = await _db.collection('tomato').doc(id).get();
     var tomatoCountInData = item?.entries.first.value ?? 0;
 
     _db.collection('tomato').doc(id).set({
       'tomatoCount': tomatoCountInData + 1,
-      'date': formatDate(DateTime.now())
+      'date': _formatDate(DateTime.now())
     });
   }
 }
