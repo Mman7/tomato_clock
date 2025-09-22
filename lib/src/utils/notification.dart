d478b157f75fb75abe3950c4b9d650a054607c55
Eric Man <youzai0427@gmail.com>
2025-09-23 03:50:42 +0800

    fix notification not working

--- lib/src/utils/notification.dart
+++ lib/src/utils/notification.dart
@@ -1,33 +1,39 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-// class NotificationService extends ChangeNotifier {
-//   final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin =
-//       FlutterLocalNotificationsPlugin();
+class NotificationService {
+  static final FlutterLocalNotificationsPlugin
+      _flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
-//   //initilize
-//   Future initialize() async {
-//     FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-//         FlutterLocalNotificationsPlugin();
-//     AndroidInitializationSettings androidInitializationSettings =
-//         const AndroidInitializationSettings('@mipmap/ic_launcher');
+  //initilize
+  static Future initialize() async {
+    FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+        FlutterLocalNotificationsPlugin();
+    AndroidInitializationSettings androidInitializationSettings =
+        const AndroidInitializationSettings('@mipmap/ic_launcher');
 
-//     final InitializationSettings initializationSettings =
-//         InitializationSettings(android: androidInitializationSettings);
+    final InitializationSettings initializationSettings =
+        InitializationSettings(android: androidInitializationSettings);
 
-//     await flutterLocalNotificationsPlugin.initialize(initializationSettings);
-//   }
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  }
 
-//   // Instant Notifications
-//   Future instantNotification() async {
-//     var android = const AndroidNotificationDetails('id', 'channel');
+  // Instant Notifications
+  static Future instantNotification() async {
+    const AndroidNotificationDetails androidPlatformChannelSpecifics =
+        AndroidNotificationDetails(
+      'id',
+      'tomato',
+      channelDescription: 'your_channel_description',
+      importance: Importance.max,
+      priority: Priority.high,
+      icon: 'ic_stat_access_alarm', // ✅ must match the resource name
+    );
+    var ios = const DarwinNotificationDetails();
 
-//     var ios = const DarwinNotificationDetails();
-
-//     var platform = NotificationDetails(android: android, iOS: ios);
-
-//     await _flutterLocalNotificationsPlugin.show(
-//         0, "Time's up Buddy !", 'Tap here recount your time', platform,
-//         payload: 'Welcome to demo app');
-//   }
-// }
+    var platform =
+        NotificationDetails(android: androidPlatformChannelSpecifics, iOS: ios);
+    await _flutterLocalNotificationsPlugin.show(
+        0, "Time's up Buddy !", 'Tap here recount your time', platform,
+        payload: 'Welcome to demo app');
+  }
+}
